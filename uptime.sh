0d81df654d5dd1465d88668f62c5c452ce79d2ed
jiajunhuang <gansteed@gmail.com>
2014-05-15 19:18:52 +0800

    fix merge conflict

--- uptime.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# display uptime for xmobar
-
-up=$(uptime -p | sed 's/up //g')
-
-
-echo $up | sed 's/minutes/min/g; s/minute/min/g; s/hours/hr/g; s/hour/hr/g; s/days/d/g; s/day/d/g; s/ //g'
--- xmobarrc.hs
+++ xmobarrc.hs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 Config {position = TopW L 90
        , font = "xft:Ubuntu Mono-10"
        , bgColor = "black"
@@ -8,68 +7,9 @@ Config {position = TopW L 90
                     , Run Date "%a %m/%d %H:%M:%S" "date" 10
                     , Run BatteryP ["BAT1"]["-t", "Batt:<left>%","-L", "10", "-H", "80", "-p", "3","--", "-O", "<fc=green>On</fc> - ", "-o", "","-L", "-15", "-H", "-5","-l", "red", "-m", "blue", "-h", "green"] 10
                     , Run Com "/home/jiajun/.xmonad/get-volume" [] "vol" 1
-                    , Run Com "/home/jiajun/.xmonad/uptime.sh" [] "uptime" 600
                     , Run StdinReader
                     ]
        , sepChar = "%"
        , alignSep = "}{"
-       , template = "%StdinReader% }{ %cpu% | %memory% | <fc=#FF0000>%battery%</fc> | Vol: <fc=green>%vol%</fc> | <fc=#e6744c>%date% </fc> | %uptime%"
+       , template = "%StdinReader% }{ %cpu% | %memory% | <fc=#FF0000>%battery%</fc> | Vol: <fc=green>%vol%</fc> | <fc=#e6744c>%date%</fc>"
        }
-=======
-Config { 
-  -- Set font and default foreground/background colors. Note that
-  -- the height of xmobar is controlled by the font you use.
-  position = TopW L 90,
-  font = "xft:Ubuntu Mono-10",
-  bgColor = "black",
-  fgColor = "grey",
-
-  -- list of commands which gather information about your system for
-  -- presentation in the bar. 
-  commands = [ 
-    -- Gather and format CPU usage information. 
-    -- If it's above 50%, we consider it high usage and make it red.  
-    Run Cpu [
-      "-H","50",
-      "--high","red"
-    ] 20,
-
-    -- Gather and format memory usage information
-    Run Memory [
-      "-t","Mem: <usedratio>%"
-    ] 20,
-
-    -- Date formatting
-    Run Date "%a %m/%d %H:%M:%S" "date" 10,
-
-    -- Battery information. This is likely to require some customization
-    -- based upon your specific hardware. Or, for a desktop you may want
-    -- to just remove this section entirely.
-    -- "-t", "<acstatus><watts> (<left>%)",
-    Run BatteryP ["BAT1"]
-            ["-t", "Batt:<left>%",
-             "-L", "10", "-H", "80", "-p", "3",
-             "--", "-O", "<fc=green>On</fc> - ", "-o", "",
-             "-L", "-15", "-H", "-5",
-             "-l", "red", "-m", "blue", "-h", "green"]
-            10,
-
-    -- This line tells xmobar to read input from stdin. That's how we
-    -- get the information that xmonad is sending it for display.
-    Run StdinReader
-   ],
-
-   -- Separator character used to wrape variables in the xmobar template 
-   sepChar = "%",
-
-   -- Alignment separater characer used in the xmobar template. Everything
-   -- before this will be aligned left, everything after aligned right.
-   alignSep = "}{",
-
-   
-
-   -- Overall template for the layout of the xmobar contents. Note that
-   -- space is significant and can be used to add padding.
-   template = "%StdinReader% }{ %cpu% | %memory% | <fc=#FF0000>%battery%</fc> | <fc=#e6744c>%date% </fc>"
-}
->>>>>>> c093d419afc2a877996ae80afe18a837af566cb6
