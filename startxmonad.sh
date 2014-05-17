d47d3b4864afb4b0d0df02aafee47974fec1b386
jiajunhuang <gansteed@gmail.com>
2014-05-18 01:46:48 +0800

    update config

--- startxmonad.sh
+++ startxmonad.sh
@@ -1,12 +1,8 @@
 #! /bin/bash
 trayer --edge top --align right --widthtype percent --width 10 --tint 0x000000 --height 19 --transparent true --alpha 0 &
 nm-applet &
-<<<<<<< HEAD
+fcitx-qimpanel &
 python /home/jiajun/Code/tools/goagent/local/goagent-gtk.py &
-=======
-python /home/jiajun/Code/myprogram/goagent/local/goagent-gtk.py &
-dropbox &
->>>>>>> c093d419afc2a877996ae80afe18a837af566cb6
 
 # autostartup apps before exec xmonad
 exec xmonad
