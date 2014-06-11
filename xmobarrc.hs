04a4ab5ce3043c3e01c081b3985d634504642379
jiajunhuang <gansteed@gmail.com>
2014-06-11 22:43:37 +0800

    修改xmobar显示

--- xmobarrc.hs
+++ xmobarrc.hs
@@ -1,15 +1,14 @@
-Config {position = TopW L 90
+Config {position = TopW L 86
        , font = "xft:Ubuntu Mono-10"
        , bgColor = "black"
        , fgColor = "grey"
        , commands = [ Run Cpu ["-H","50","--high","red"] 20
                     , Run Memory ["-t","Mem: <usedratio>%"] 20
-                    , Run Date "%a %m/%d %H:%M:%S" "date" 10
-                    , Run BatteryP ["BAT1"]["-t", "Batt:<left>%","-L", "10", "-H", "80", "-p", "3","--", "-O", "<fc=green>On</fc> - ", "-o", "","-L", "-15", "-H", "-5","-l", "red", "-m", "blue", "-h", "green"] 10
+                    , Run Date "%m/%d %H:%M" "date" 10
                     , Run Com "/home/jiajun/.xmonad/get-volume" [] "vol" 1
                     , Run StdinReader
                     ]
        , sepChar = "%"
        , alignSep = "}{"
-       , template = "%StdinReader% }{ %memory% | <fc=#FF0000>%battery%</fc> | Vol: <fc=green>%vol%</fc> | <fc=#e6744c>%date%</fc>"
-       }
+                      , template = "%StdinReader% }{ %memory% | Vol: <fc=green>%vol%</fc> | <fc=#e6744c>%date%</fc>  "
+                      }
