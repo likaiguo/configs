07dd7847e00192573440cb5e746d2b3455d87c59
jiajunhuang <gansteed@gmail.com>
2014-06-11 22:45:00 +0800

    修改登入启动程序

--- startxmonad.sh
+++ startxmonad.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 trayer --edge top --align right --widthtype percent --width 10 --tint 0x000000 --height 19 --transparent true --alpha 0 &
+xfce4-power-manager &
 nm-applet &
 fcitx-qimpanel &
 python /home/jiajun/Code/tools/goagent/local/goagent-gtk.py &
+dropbox start &
 
 # autostartup apps before exec xmonad
 exec xmonad
