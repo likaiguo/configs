beef31838eb6315c498b39a763defff3293f6a14
jiajunhuang <gansteed@gmail.com>
2014-08-25 09:35:40 +0800

    add goldendict with login xmonad. add pidgin layout in xmonad.

--- startxmonad.sh
+++ startxmonad.sh
@@ -2,9 +2,11 @@
 trayer --edge top --align right --widthtype percent --width 10 --tint 0x000000 --height 19 --transparent true --alpha 0 & # spawn trayer
 syndaemon -i 1 & # touchpad
 python /home/jiajun/Code/tools/goagent/local/goagent-gtk.py & # GFW
-
+compton &
 # Fire up apps
-gnome-screensaver&
+gnome-screensaver &
+sogou-qimpanel &
+goldendict &
 
 #start laptop's network manager
 if [ -x /usr/bin/nm-applet ] ; then
